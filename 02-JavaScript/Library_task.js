27df8a1bae497f5556748868d1378db18c249dfe
Mostafa <mostafa@Mostafas-MacBook-Pro.local>
2026-02-15 22:34:16 +0200

    Added the functions returnBook & showReport

--- 02-JavaScript/Library_task.js
+++ 02-JavaScript/Library_task.js
@@ -22,10 +22,10 @@ const choice = prompt(
             borrowBook()
             break;
         case "3":
-            alert("You chose to return a book");
+            returnBook()
             break;
         case "4":
-            alert("You chose to show library report");
+            showReport();
             break;
         case "5":
             alert("You chose to exit the system.");
@@ -88,5 +88,77 @@ function borrowBook() {
 
 
 }
+
+function returnBook() {
+
+    let searchTitle = prompt("Enter the title of the book to return:");
+    if (!searchTitle) {
+        alert("Please enter a valid title.");
+        return;}
+        let foundBook = null;
+        for (let book of library) {
+            if (book.title.toLowerCase() === searchTitle.toLowerCase().trim()) {
+                foundBook = book;
+                break;
+
+            }
+        }
+        if (!foundBook) {
+
+            alert("Book not found.");
+            
+        }else if (foundBook.borrowedCopies === 0) {
+            alert("This book is already fully stocked. Are you sure you borrowed it from here?");
+        }else{
+            foundBook.availableCopies = foundBook.availableCopies +1;
+            foundBook.borrowedCopies = foundBook.borrowedCopies -1;
+            alert("Book returned successfully: " + foundBook.title);
+        }
+    }
+
+function calculateTotalBooks(){
+    let totalAvailable = 0;
+    let totalBorrowed = 0;
+    for (let book of library) {
+        totalAvailable += book.availableCopies;
+        totalBorrowed += book.borrowedCopies;
+    }
+    return{
+        available : totalAvailable,
+        borrowed: totalBorrowed
+    };
+
+}
+
+function showReport() {
+    let totals = calculateTotalBooks();
+    let report = "=== LIBRARY REPORT ===\n\n";
+    if (library.length === 0) {
+        report += "The library is empty.";
+
+
+    }else{
+        for (let book of library) {
+            let status = "Available";
+            if (book.availableCopies === 0) {
+                status = "Not Available";
+            }else if (book.availableCopies<3) {
+                status= "Limited Stock";
+            }
+            report += `Title: ${book.title}\n`;
+            report += `Author: ${book.author}\n`;
+            report += `Available: ${book.availableCopies} | Borrowed: ${book.borrowedCopies}\n`;
+            report += `Status: ${status}\n`;
+            report += "------------------------\n";
+
+        }
+    }
+    report += "\n=== TOTALS ===\n";
+    report += `Total Available Books: ${totals.available}\n`;
+    report += `Total Borrowed Books: ${totals.borrowed}\n`;
+
+    alert(report);
+}
+
 
 runLibrary()
